50c4e78a3d127576b42b1ea94acb0a42d6c5f2df
Avanade <Avanade@WIN-Z8P0HYD8IO.corp.avanade.org>
2020-10-17 16:47:30 -0300

    Estrutura de App criado

---
+++ OverviewCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OverviewCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
